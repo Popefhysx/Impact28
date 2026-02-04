fcdfca080105c4915219e5872dd898f9816f826f
Pope <lyricspenpad@gmail.com>
2026-02-04 13:30:43 +0100

    fix: resolve circular dependency with forwardRef
    StaffModule -> EmailModule -> NotificationsModule -> StaffModule
    
    Used forwardRef() in NotificationsModule to break the cycle.

--- impact-os-backend/src/notifications/notifications.module.ts
+++ impact-os-backend/src/notifications/notifications.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { NotificationRoutingService } from './notification-routing.service';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
@@ -6,9 +6,10 @@ import { PrismaModule } from '../prisma';
 import { StaffModule } from '../staff/staff.module';
 
 @Module({
-    imports: [PrismaModule, StaffModule],
+    imports: [PrismaModule, forwardRef(() => StaffModule)],
     controllers: [NotificationsController],
     providers: [NotificationRoutingService, NotificationsService],
     exports: [NotificationRoutingService, NotificationsService],
 })
 export class NotificationsModule { }
+
