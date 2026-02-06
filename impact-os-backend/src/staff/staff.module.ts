d54fb712db8afa2a95d72f5228bf211fd70b3082
Pope <lyricspenpad@gmail.com>
2026-02-06 07:07:56 +0100

    fix(modules): resolve circular dependency in StaffModule
    Added forwardRef() around EmailModule import to break the circular chain:
    StaffModule -> EmailModule -> NotificationsModule -> StaffModule
    
    This was causing 'UndefinedModuleException' at runtime.

--- impact-os-backend/src/staff/staff.module.ts
+++ impact-os-backend/src/staff/staff.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
 import { PrismaModule } from '../prisma';
 import { EmailModule } from '../email';
 
 @Module({
-  imports: [PrismaModule, EmailModule],
+  imports: [PrismaModule, forwardRef(() => EmailModule)],
   controllers: [StaffController],
   providers: [StaffService],
   exports: [StaffService],
 })
-export class StaffModule {}
+export class StaffModule { }
+
